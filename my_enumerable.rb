7b6b4737d4c339076ade4ec51d470c1f4355cef4
Katrina <blueyed_qt@hotmail.com>
2015-11-21 15:01:31 -0800

    Added my_each function

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,2 +1,16 @@
 module MyEnumerable
+	def my_each
+		if block_given?
+			puts self.each { |e| yield(e) }
+		end
+	end
 end
+
+class Array
+	include MyEnumerable 
+end
+
+# arr = [12, 24, 8, 9]
+# output = []
+# arr.my_each { |x| x }
+# 	#output << x + 20 }
